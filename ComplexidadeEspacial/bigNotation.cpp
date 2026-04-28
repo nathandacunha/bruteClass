aced02b6993786643c3bb89dce56aa04a1424cb1
nathandacunha <nathandacunha233@gmail.com>
2026-04-28 17:54:07 -0300

    Estrutura de Dados: Complexidade de Algoritmos

---
+++ ComplexidadeEspacial/bigNotation.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int soma(int n) {
+    int resultado = 0; // Apenas uma variável inteira
+    for(int i = 0; i < n; i++) {
+        resultado += i;
+    }
+    return resultado;
+}
