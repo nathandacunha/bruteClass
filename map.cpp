2017ab7c78bf5315d940b9aa61a3eead14f4587a
nathandacunha <nathandacunha233@gmail.com>
2026-05-05 17:54:01 -0300

    conceito de map em c++

---
+++ map.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    // semelhante ao objetos em js
+
+    // map<string, int> mapa;
+    // mapa["um"] = 1;
+    // mapa["dois"] = 2;
+    // mapa["tres"] = 100;
+
+    // mapa["um"]++;
+    // mapa["um"]++;
+    // mapa["um"]++;
+    
+
+
+    // cout << mapa["um"] << endl;
+
+    // é possivel usar map como um array
+
+    int n;
+    cin >> n;
+
+    map<int, int> mapa;
+    for(int i = 0; i < n; i++) {
+        cin >> mapa[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << mapa[i] << endl;
+    }
+    return 0;
+}
